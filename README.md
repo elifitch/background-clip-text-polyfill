b24993ff3f9c81f2a4a63c1fb8278dc12f808ee4
eli <eli.fitch.prof@gmail.com>
2015-01-17 10:15:36 -0500

    trying this in canvas

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 #background-clip-text: polyfill
-A simple, fire and forget polyfill that, gives a clip text effect on browsers that don't support -webkit-background-clip: text.  The polyfill builds an SVG that mimics the look of background-clip-text.  All you need to feed the polyfill is a target element, it takes care of the rest.
+A simple, fire and forget polyfill that, gives a clip text effect on browsers that don't support -webkit-background-clip: text.  The polyfill builds a canvas that mimics the look of background-clip-text.  All you need to feed the polyfill is a target element, it takes care of the rest.
--- js/polyfill.js
+++ js/polyfill.js
@@ -2,11 +2,49 @@
 
 	Element.prototype.bgClipPolyfill = function(){
 		if(!document.body.style.webkitBackgroundClip) {
-			var style = window.getComputedStyle(this),
-					bg = style.backgroundImage.match(/url\((?:\s*?"\s*?|\s*?'\s*?)(.+?)(?=\s*?"\s*?|\s*?'\s*?|\s*?\)\s*?)/g)[0].replace(/url(?:\s*?\("\s*?|\s*?\('\s*?|\s*?\(\s*?)/g,'');
-			
-			console.log(bg)
+			var el = this,
+					style = window.getComputedStyle(el),
+					bg = style.backgroundImage.match(/url\((?:\s*?"\s*?|\s*?'\s*?)(.+?)(?=\s*?"\s*?|\s*?'\s*?|\s*?\)\s*?)/g)[0].replace(/url(?:\s*?\("\s*?|\s*?\('\s*?|\s*?\(\s*?)/g,''),
+					fontSize = style.fontSize,
+					fontFamily = style.fontFamily,
+					fontStyle = style.fontStyle,
+					fontWeight = style.fontWeight
+					lineHeight = style.lineHeight;
 
+			// this.parent.insertBefore('<div style="background: red; width: 100%; height: 50px;', parent.firstChild);
+			canvas = document.createElement('canvas');
+			
+			var ctx = canvas.getContext("2d");
+			//create image we're going to use as a fill pattern
+			var img = document.createElement("img");
+			img.src = bg;
+			//draw the text
+			img.onload= function(){
+				createCanvas();
+			  drawText();
+			  console.log('image loaded')
+			}
+			//function that draws the text and fills it with the texture from the img
+			function drawText() {
+			  
+
+			  ctx.font = fontStyle+' normal '+fontWeight+' '+fontSize+' '+fontFamily;
+
+			  ctx.fillStyle = ctx.createPattern(img, 'repeat');
+			  console.log(ctx.fillStyle)
+			  ctx.textAlign = 'center';
+			  var x = canvas.width / 2; 
+			  ctx.fillText("butthole", x, 400);
+			  ctx.fill();
+			}
+			function createCanvas(){
+				el.parentNode.insertBefore(canvas, parent.firstChild);
+				el.parentNode.removeChild(el);
+			  canvas.width = 1000;
+			  canvas.height = 400;
+			}
+
+			
 		}
 	}
 
