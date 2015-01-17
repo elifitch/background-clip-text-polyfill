dda73f43a3600f0ce0c4f5192a6a54d6785de2be
eli <eli.fitch.prof@gmail.com>
2015-01-17 17:56:21 -0500

    progress

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 #background-clip-text: polyfill
 A simple, fire and forget polyfill that, gives a clip text effect on browsers that don't support -webkit-background-clip: text.  The polyfill builds an SVG that mimics the look of background-clip-text.  All you need to feed the polyfill is a target element, it takes care of the rest.
+
+##To-dos
+ - Handle text align
+ - Handle multiline text
+ - Background size
--- js/polyfill.js
+++ js/polyfill.js
@@ -2,10 +2,53 @@
 
 	Element.prototype.bgClipPolyfill = function(){
 		if(!document.body.style.webkitBackgroundClip) {
-			var style = window.getComputedStyle(this),
+			var el = this,
+					doc = document,
+					style = window.getComputedStyle(el),
 					bg = style.backgroundImage.match(/url\((?:\s*?"\s*?|\s*?'\s*?)(.+?)(?=\s*?"\s*?|\s*?'\s*?|\s*?\)\s*?)/g)[0].replace(/url(?:\s*?\("\s*?|\s*?\('\s*?|\s*?\(\s*?)/g,'');
+
+			function makeSVG() {
+				var svg = doc.createElement('svg'),
+						defs = doc.createElement('defs'),
+						clipPath = doc.createElement('clippath'),
+						text = doc.createElement('text'),
+						image = doc.createElement('image');
+
+				clipPath.setAttribute('id', 'clip-path');
+
+				//give el classes to <text>
+				text.setAttribute('class', el.getAttribute('class'));
+
+				//image attrs
+				image.setAttribute('xlink:href', bg);
+				image.setAttribute('width', '100%');
+				image.setAttribute('height', '100%');
+				image.setAttribute('clip-path', 'url(#clip-path)')
+				
+				//set up SVG structure
+				svg.appendChild(defs).appendChild(clipPath).appendChild(text);
+				svg.appendChild(image);
+
+				//insert SVG
+				el.parentNode.insertBefore(svg, el);
+				//remove el
+			}
+			makeSVG();
 			
-			console.log(bg)
+// 			<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="1200" height="400" viewBox="0 0 1200 400"> 
+//     <!-- add title for accessibility -->
+//     <title>Adding Background to Text using SVG clipPath</title> 
+//     <defs>  
+//       <clippath id="my-path">
+//         <text x="50" y="300" font-size="200" >WATERCOLOR</text>
+//       </clippath>
+//     </defs>
+//     <image xlink:href="img/watercolor_1.jpg" clip-path="url(#test)" width="100%" height="100%" preserveAspectRatio="none" />
+// </svg>
+
+// image {
+//     clip-path: url(#my-path);
+// }
 
 		}
 	}
