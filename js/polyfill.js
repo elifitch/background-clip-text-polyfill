afe9d3aff1aea808c8537c22ac5ba62715b22934
eli <eli.fitch.prof@gmail.com>
2015-01-16 22:53:45 -0500

    reorganizing

--- js/polyfill.js
+++ js/polyfill.js
@@ -3,12 +3,9 @@
 	Element.prototype.bgClipPolyfill = function(){
 		if(!document.body.style.webkitBackgroundClip) {
 			var style = window.getComputedStyle(this),
-					bg = style.backgroundImage,
-					//remove trailing ')
-					bgTrimTrailing = bg.match(/url\((?:\s*?"\s*?|\s*?'\s*?)(.+?)(?=\s*?"\s*?|\s*?'\s*?|\s*?\)\s*?)/g)[0],
-					bgTrimLeading = bgTrimTrailing.replace(/url(?:\s*?\("\s*?|\s*?\('\s*?|\s*?\(\s*?)/g,'');
-			console.log(bgTrimLeading)
-			// console.log('foxy')
+					bg = style.backgroundImage.match(/url\((?:\s*?"\s*?|\s*?'\s*?)(.+?)(?=\s*?"\s*?|\s*?'\s*?|\s*?\)\s*?)/g)[0].replace(/url(?:\s*?\("\s*?|\s*?\('\s*?|\s*?\(\s*?)/g,'');
+			
+			console.log(bg)
 
 		}
 	}
